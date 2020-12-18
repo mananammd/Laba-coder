5a088e990675fefcadc3a796079b2e71fc03b818
Манана <76215627+mananammd@users.noreply.github.com>
2020-12-18 22:06:53 +0300

    Update encoder.c

--- encoder.c
+++ encoder.c
@@ -54,13 +54,13 @@ int main() {
 	int offset;
 	printf("Enter offset: ");
 	scanf("%d", &offset);
-	printf("%s\n", CaesarEncoder(text, offset));
+	printf("%s\n", mutableCaesarEncoder(text, offset));
     }
     else if (WhatCipher(cipher, "XOR") == 1) {
 	char password[200];
 	printf("Enter password: ");
        	scanf("%s", password);
-        printf("%s\n", XorEncoder(text, password));
+        printf("%s\n", mutableXorEncoder(text, password));
     }   
     else {
 	printf("Incorrect data");
