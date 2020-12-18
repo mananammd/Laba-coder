9c8e84e5e71e9441f238cdf9c01404888d23b066
Манана <76215627+mananammd@users.noreply.github.com>
2020-12-18 20:15:17 +0300

    Update encoder.c

--- encoder.c
+++ encoder.c
@@ -8,9 +8,8 @@ char* CaesarEncoder(const char* str, int offset) {
     char* StrRes = immutableStrip(str);
     mutableToLower(StrRes);
     for (int i = 0; i < strlen(StrRes); ++i) {
-	    StrRes[i] = (str[i] + (offset % 26));   
+	StrRes[i] = (str[i] + (offset % 26));   
     }
-
     return StrRes;
 }
 
@@ -23,7 +22,6 @@ char* XorEncoder(const char* str, const char* password) {
 	StrRes[i] = (StrRes[i] ^ password[k]);
 	k = (k + 1) % strlen(password);
     }
-
     return StrRes;
 }
 
@@ -37,20 +35,19 @@ int main() {
     scanf("%s", text);
 
     if (WhatCipher(cipher, "Caesar") == 1) {
-	    int offset;
-	    printf("Enter offset: ");
-	    scanf("%d", &offset);
-	    printf("%s\n", CaesarEncoder(text, offset));
+	int offset;
+	printf("Enter offset: ");
+	scanf("%d", &offset);
+	printf("%s\n", CaesarEncoder(text, offset));
     }
-	else if (WhatCipher(cipher, "XOR") == 1) {
-	    char password[200];
-	    printf("Enter password: ");
+    else if (WhatCipher(cipher, "XOR") == 1) {
+	char password[200];
+	printf("Enter password: ");
        	scanf("%s", password);
         printf("%s\n", XorEncoder(text, password));
-    }
-        
+    }   
     else {
-	    printf("Incorrect data");
+	printf("Incorrect data");
     }
 
     return 0;
