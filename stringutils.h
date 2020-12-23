0301add1e35e59ab0191d9d87341f4120c9ec61e
Манана <76215627+mananammd@users.noreply.github.com>
2020-12-23 12:23:26 +0300

    Update stringutils.h

--- stringutils.h
+++ stringutils.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int WhatCipher(const char* type, const char* ciph);
+int whatCipher(const char* type, const char* ciph);
 
 void mutableToUpper(char* str);
 char* immutableToUpper(const char* str);
